498aeed2b89b87360415c56f1d6a2327584da9da
JKamue <mail@jkamue.de>
2020-05-29 14:07:50 +0200

    Moved Panel, added Framerate and panel object interface

---
+++ SpaceFlight/Objects/FrameRate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceFlight.Objects
+{
+    class FrameRate
+    {
+        public int Framerate { get; private set} = 0;
+
+        private long unixStamp = 0;
+        private int lastFrames = 0;
+
+        public FrameRate(int fps)
+        {
+            Framerate = fps;
+        }
+
+        public FrameRate()
+        {
+            unixStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+
+        public void FrameDrawn()
+        {
+            if (unixStamp == DateTimeOffset.UtcNow.ToUnixTimeSeconds())
+            {
+                lastFrames++;
+            } else
+            {
+                unixStamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                Framerate = lastFrames;
+                lastFrames = 1;
+            }
+
+        }
+
+        public int CalculateDelay() => (int) Math.Floor(1000 / (float) Framerate);
+    }
+}
---
+++ SpaceFlight/Panel/PanelController.cs
@@ -0,0 +1,39 @@
+﻿using SpaceFlight.Objects;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceFlight
+{
+    class PanelController
+    {
+        private readonly Panel _panel;
+
+        private FrameRate targetFramerate;
+        private FrameRate actualFramerate;
+
+        private readonly List<IPanelObject> _panelObjects;
+
+        public PanelController(Panel panel, FrameRate frameRate)
+        {
+            _panel = panel;
+            _panelObjects = new List<IPanelObject>();
+            targetFramerate = frameRate;
+
+            actualFramerate = new FrameRate();
+        }
+
+        public void AddPanelObject(IPanelObject o) => _panelObjects.Add(o);
+
+        public void RemovePanelObject(IPanelObject o) => _panelObjects.RemoveAll(x => x == o);
+
+        public void SetTargetFramerate(FrameRate f) => targetFramerate = f;
+
+        public int GetActualFramerate() => actualFramerate.Framerate;
+
+    }
+}
