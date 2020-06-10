550401f1fdea6aab3793de796a806cf17c74a31d
JKamue <mail@jkamue.de>
2020-06-10 13:09:08 +0200

    Added tests for force class

--- SpaceFlight/Physics/Units/Force.cs
+++ SpaceFlight/Physics/Units/Force.cs
@@ -12,12 +12,16 @@ namespace SpaceFlight.Physics
     public class Force
     {
         public Angle Angle { get; }
-        private double _force;
+        public double Value { get;}
+
+        public double Pow => Math.Pow(Value, 2);
+        public double Rad => Angle.Radian;
+        public double Deg => Angle.Degree;
 
         public Force(Angle angle, double newton)
         {
             Angle = angle;
-            _force = newton;
+            Value = newton;
         }
 
         public static Force operator +(Force f1, Force f2)
@@ -29,12 +33,12 @@ namespace SpaceFlight.Physics
                 f2 = t;
             }
 
-            if (f1.Val == 0)
-                return new Force(f2.Angle, f2.Val);
+            if (f1.Value == 0)
+                return new Force(f2.Angle, f2.Value);
 
             var alpha = f1.Rad - f2.Rad;
-            var resultingForce = Math.Sqrt(f1.Pow + f2.Pow + 2 * f1.Val * f2.Val * Math.Cos(alpha));
-            var resultingAngle = Math.Acos((f1.Pow + Math.Pow(resultingForce, 2) - f2.Pow) / (2 * f1.Val * resultingForce)) *
+            var resultingForce = Math.Sqrt(f1.Pow + f2.Pow + 2 * f1.Value * f2.Value * Math.Cos(alpha));
+            var resultingAngle = Math.Acos((f1.Pow + Math.Pow(resultingForce, 2) - f2.Pow) / (2 * f1.Value * resultingForce)) *
                 180 / Math.PI;
 
             if (f2.Deg - f1.Deg >= 180)
@@ -60,12 +64,6 @@ namespace SpaceFlight.Physics
 
             return new Force(angle, resultingForce);
         }
-
-        
-        public double Pow => Math.Pow(_force, 2);
-        public double Val => _force;
-        public double Rad => Angle.Radian;
-        public double Deg => Angle.Degree;
     }
 
 }
