7182add38bd8342e73f9e1d18a4a55a5a7f9f159
JKamue <mail@jkamue.de>
2020-06-10 21:23:17 +0200

    Added tests for spee class

---
+++ PhysicsTests/SpeedClassTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpaceFlight.Physics.Units;
+
+namespace PhysicsTests
+{
+    [TestClass]
+    public class SpeedClassTest
+    {
+        [TestMethod]
+        public void SpeedIsAddable()
+        {
+            var s1 = new Speed(30);
+            var s2 = new Speed(10);
+            Assert.AreEqual(40, (s1 + s2).Value);
+        }
+
+        [TestMethod]
+        public void SpeedIsSubtractable()
+        {
+            var s1 = new Speed(30);
+            var s2 = new Speed(10);
+            Assert.AreEqual(20, (s1 - s2).Value);
+        }
+
+        [TestMethod]
+        public void SpeedCanBeConvertedToDistance()
+        {
+            var s = new Speed(30);
+            var span = TimeSpan.FromSeconds(0.3);
+            Assert.AreEqual(9, s.GetDistance(span));
+        }
+    }
+}
--- SpaceFlight/Physics/Units/Speed.cs
+++ SpaceFlight/Physics/Units/Speed.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
 namespace SpaceFlight.Physics.Units
 {
@@ -13,6 +14,21 @@ namespace SpaceFlight.Physics.Units
         public Speed(double value)
         {
             Value = value;
+        }
+
+        public static Speed operator +(Speed s1, Speed s2)
+        {
+            return new Speed(s1.Value + s2.Value);
+        }
+
+        public static Speed operator -(Speed s1, Speed s2)
+        {
+            return new Speed(s1.Value - s2.Value);
+        }
+
+        public double GetDistance(TimeSpan span)
+        {
+            return Value * span.TotalSeconds;
         }
     }
 }
