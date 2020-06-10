83399e6f71d22395801f1becea3d56686b01ca18
JKamue <mail@jkamue.de>
2020-06-10 13:20:31 +0200

    Added Acceleration class

---
+++ SpaceFlight/Physics/Units/Acceleration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceFlight.Physics.Units
+{
+    public class Acceleration
+    {
+        public double Value { get; }
+
+        public Acceleration(double value)
+        {
+            Value = value;
+        }
+    }
+}
