da5ff8ca5878425cce7cabd1cb196fe424f63007
JKamue <mail@jkamue.de>
2020-06-10 13:20:45 +0200

    Added Mass class

---
+++ SpaceFlight/Physics/Units/Mass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceFlight.Physics.Units
+{
+    public class Mass
+    {
+        public double Value { get; }
+
+        public Mass(double value)
+        {
+            Value = value;
+        }
+
+        public static Mass operator +(Mass m1, Mass m2)
+        {
+            return new Mass(m1.Value + m2.Value);
+        }
+
+        public static Mass operator -(Mass m1, Mass m2)
+        {
+            return new Mass(m1.Value - m2.Value);
+        }
+    }
+}
