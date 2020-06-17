326dfd8df567aca49fbb108124855a424980980a
JKamue <mail@jkamue.de>
2020-06-17 18:48:43 +0200

    Added DragProperties and tests

---
+++ PhysicsTests/DragPropertiesTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpaceFlight.Physics.Other;
+using SpaceFlight.Physics.Units;
+
+namespace PhysicsTests
+{
+    [TestClass]
+    public class DragPropertiesTest
+    {
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly1()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(10, p.GetArea(Angle.FromDegrees(0)));
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly2()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(100, p.GetArea(Angle.FromDegrees(90)));
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly3()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(10, p.GetArea(Angle.FromDegrees(180)));
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly4()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(100, p.GetArea(Angle.FromDegrees(270)));
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly5()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(55, p.GetArea(Angle.FromDegrees(45)), 0.001);
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly6()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(55, p.GetArea(Angle.FromDegrees(135)), 0.001);
+        }
+
+        [TestMethod]
+        public void AreaIsCalculatedCorrectly7()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(55, p.GetArea(Angle.FromDegrees(315)), 0.001);
+        }
+
+        [TestMethod]
+        public void DragCoefficientIsCorrectly1()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(0.25, p.GetDragCoefficient(Angle.FromDegrees(0)), 0.001);
+        }
+
+        [TestMethod]
+        public void DragCoefficientIsCorrectly2()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(0.41, p.GetDragCoefficient(Angle.FromDegrees(90)), 0.001);
+        }
+
+        [TestMethod]
+        public void DragCoefficientIsCorrectly3()
+        {
+            var p = new DragProperties(0.25, 0.41, 10, 100);
+            Assert.AreEqual(0.33, p.GetDragCoefficient(Angle.FromDegrees(315)), 0.001);
+        }
+    }
+}
---
+++ SpaceFlight/Physics/Other/DragProperties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceFlight.Physics.Units;
+
+namespace SpaceFlight.Physics.Other
+{
+    public class DragProperties
+    {
+        private readonly double _cdUp;
+        private readonly double _cdSide;
+        private readonly double _areaUp;
+        private readonly double _areaSide;
+
+        public DragProperties(double cdUp, double cdSide, double areaUp, double areaSide)
+        {
+            _cdUp = cdUp;
+            _cdSide = cdSide;
+            _areaUp = areaUp;
+            _areaSide = areaSide;
+        }
+
+        private static double MapAngle(Angle a) => Math.Cos(2 * a.Radian);
+
+        private static double Map(double value, double from1, double to1, double from2, double to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+        public double GetArea(Angle a)
+        {
+            var value = MapAngle(a);
+            return Map(value, 1, -1, _areaUp, _areaSide);
+        }
+
+        public double GetDragCoefficient(Angle a)
+        {
+            var value = MapAngle(a);
+            return Map(value, 1, -1, _cdUp, _cdSide);
+        }
+    }
+}
