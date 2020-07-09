22858349b7658115b22b0989a0b65830ca367cd2
JKamue <mail@jkamue.de>
2020-07-09 22:25:39 +0200

    Added more zoom constants, closing #18

--- SpaceFlight/Physics/Other/TimeKeeper.cs
+++ SpaceFlight/Physics/Other/TimeKeeper.cs
@@ -10,24 +10,22 @@ namespace SpaceFlight.Physics.Other
 {
     static class TimeKeeper
     {
-        private static double microSecondsSinceStart = 0;
-        private static DateTime lastCheck = DateTime.Now;
-        private static float timeConstant = 1;
+        private static double _microSecondsSinceStart = 0;
+        private static DateTime _lastCheck = DateTime.Now;
+        public static float TimeConstant = 1;
 
         public static TimeSpan Now()
         {
             UpdateMilisecondsSinceStart();
-            return new TimeSpan((long) microSecondsSinceStart * 100);
+            return new TimeSpan((long) _microSecondsSinceStart * 100);
         }
-
-        public static void SetConstant(float i) => timeConstant = i;
 
         private static void UpdateMilisecondsSinceStart()
         {
-            var diff = (DateTime.Now - lastCheck).TotalMilliseconds * 100 * timeConstant;
-            microSecondsSinceStart += (microSecondsSinceStart + diff < 0) ? 0 : diff;
+            var diff = (DateTime.Now - _lastCheck).TotalMilliseconds * 100 * TimeConstant;
+            _microSecondsSinceStart += (_microSecondsSinceStart + diff < 0) ? 0 : diff;
             
-            lastCheck = DateTime.Now;
+            _lastCheck = DateTime.Now;
         }
     }
 }
--- SpaceFlight/Screen/InfoScreen.cs
+++ SpaceFlight/Screen/InfoScreen.cs
@@ -162,26 +162,32 @@ namespace SpaceFlight.Screen
             switch (playBackSpeed)
             {
                 case 1:
-                    playBackSpeed++;
-                    TimeKeeper.SetConstant(2);
-                    btnSetPlaybackSpeed.Text = "2X";
+                    TimeKeeper.TimeConstant = 2;
                     break;
                 case 2:
-                    playBackSpeed++;
-                    TimeKeeper.SetConstant(0.5F);
-                    btnSetPlaybackSpeed.Text = "1/2X";
+                    TimeKeeper.TimeConstant = 5;
                     break;
                 case 3:
-                    playBackSpeed++;
-                    TimeKeeper.SetConstant(-1F);
-                    btnSetPlaybackSpeed.Text = "-1X";
+                    TimeKeeper.TimeConstant = 10;
                     break;
                 case 4:
-                    playBackSpeed = 1;
-                    TimeKeeper.SetConstant(1);
-                    btnSetPlaybackSpeed.Text = "1X";
+                    TimeKeeper.TimeConstant = 25;
+                    break;
+                case 5:
+                    TimeKeeper.TimeConstant = 50;
+                    break;
+                case 6:
+                    TimeKeeper.TimeConstant = 100;
+                    break;
+                case 7:
+                    playBackSpeed = -1;
+                    TimeKeeper.TimeConstant = 0;
                     break;
             }
+
+            playBackSpeed++;
+
+            btnSetPlaybackSpeed.Text = TimeKeeper.TimeConstant + "X";
         }
     }
 }
