53de7858aa65f5e01b562b0e5d927b599ba279ab
PhilHawk03 <37216677+PhilHawk03@users.noreply.github.com>
2020-07-10 14:49:38 +0200

    Update AtmosphereCalculator.cs
    Ich weiß, dass ist echt hässlich und funktioniert nicht, aber so stelle ich mir das in etwa vor

--- SpaceFlight/Physics/Calculator/AtmosphereCalculator.cs
+++ SpaceFlight/Physics/Calculator/AtmosphereCalculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,34 +10,46 @@ namespace SpaceFlight.Physics.Calculator
         public static double CalculateAirDensityAtAltitude(double altitude)
         {
 
-            // Sorry for this
-            var densityAtAltitude = new Dictionary<Func<double, bool>, double>
+            bool altitude < 10000 = true;
+
+            if (condition)
             {
-                { x => x < 0, 0},
-                { x => x < 1000, 1.225},
-                { x => x < 2000, 1.112},
-                { x => x < 3000, 1.007},
-                { x => x < 4000, 0.9093},
-                { x => x < 5000, 0.8194},
-                { x => x < 6000, 0.7364},
-                { x => x < 7000, 0.6601},
-                { x => x < 8000, 0.5900},
-                { x => x < 9000, 0.5258},
-                { x => x < 10000, 0.4671},
-                { x => x < 15000, 0.4135},
-                { x => x < 20000, 0.1948},
-                { x => x < 25000, 0.08891},
-                { x => x < 30000, 0.04008},
-                { x => x < 40000, 0.01841},
-                { x => x < 50000, 0.003996},
-                { x => x < 60000, 0.001027},
-                { x => x < 70000, 0.0003097},
-                { x => x < 80000, 0.00008283},
-                { x => x < 90000, 0.00001846},
-                { x => x >= 90000, 0},
-            };
+                var densityAtAltitude = 1,2526 * 0,9999 ^ altitude;
+            }
+            bool altitude > 10000 and altitude < 300000  = true;
+
+            if (condition)
+            {
+                var densityAtAltitude = 1.9971 * 0,9998 ^ altitude;
+            }
+            bool altitude > 30000 and altitude< 80000 = true;
+
+            if (condition)
+            {
+                var densityAtAltitude = 0,9786 * 0.9999 ^ altitude;
+            }
+
+            bool altitude > 80000 = true;
+
+            if (condition)
+            {
+                var densityAtAltitude = 0;
+            }
+        }
+
+
+
+
+
+
+ 
+
+
+
+
 
             return densityAtAltitude.First(sw => sw.Key(altitude)).Value;
         }
     }
 }
+
