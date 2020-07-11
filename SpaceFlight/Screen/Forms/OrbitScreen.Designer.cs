ab90a32295d7e21b3dec9a2e6d1c8d26a8006b59
JKamue <mail@jkamue.de>
2020-07-11 19:19:25 +0200

    Added logo and full sized panel to OrbitScreen #15

--- SpaceFlight/Screen/Forms/OrbitScreen.Designer.cs
+++ SpaceFlight/Screen/Forms/OrbitScreen.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrbitScreen));
+            this.pnlOrbitScreen = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnlOrbitScreen
+            // 
+            this.pnlOrbitScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlOrbitScreen.Location = new System.Drawing.Point(0, 0);
+            this.pnlOrbitScreen.Name = "pnlOrbitScreen";
+            this.pnlOrbitScreen.Size = new System.Drawing.Size(503, 440);
+            this.pnlOrbitScreen.TabIndex = 0;
             // 
             // OrbitScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(503, 440);
+            this.Controls.Add(this.pnlOrbitScreen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "OrbitScreen";
@@ -46,5 +58,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlOrbitScreen;
     }
 }
